68102ba767f5f5a26d3da5882fd2a78ce3dd799b
OliverDorrian <dorrianolfc@gmail.com>
2022-03-18 22:49:18 +0000

    inital upload

---
+++ index.js
@@ -0,0 +1,50 @@
+
+var itemBut = document.querySelectorAll('.buttonID');
+
+function disableBodyScroll(){
+    const element = document.querySelector("#appBody");
+    element.classList.add("stopScroll");
+}
+   
+function enableBodyScroll(){
+    const element = document.querySelector("#appBody");
+    element.classList.remove("stopScroll");
+}
+
+document.getElementById("xSymbol").addEventListener("click", function(){
+    document.getElementById("loginOverlay").style.visibility = "hidden";
+    document.getElementById("loginOverlay").style.position = "absolute";
+    document.getElementById("signUp").style.visibility = "hidden";
+    document.getElementById("signUp").style.position = "absolute";
+    document.getElementById("login").style.visibility = "hidden";
+    document.getElementById("login").style.position = "absolute";
+    enableBodyScroll()
+});
+
+document.getElementById("signUpLoad").addEventListener("click", function(){
+    document.getElementById("login").style.visibility = "hidden";
+    document.getElementById("login").style.position = "absolute";
+
+    document.getElementById("signUp").style.visibility = "visible";
+    document.getElementById("signUp").style.position = "absolute";
+});
+
+
+document.getElementById("loginLoad").addEventListener("click", function(){
+    document.getElementById("login").style.visibility = "visible";
+    document.getElementById("login").style.position = "absolute";
+
+    document.getElementById("signUp").style.visibility = "hidden";
+    document.getElementById("signUp").style.position = "absolute";
+});
+
+document.getElementById("topBarLogin").addEventListener("click", function(){
+    document.getElementById("loginOverlay").style.visibility = "visible";
+    document.getElementById("login").style.position = "fixed";
+    document.getElementById("login").style.visibility = "visible";
+    document.getElementById("login").style.position = "absolute";
+
+    disableBodyScroll()
+});
+
+
